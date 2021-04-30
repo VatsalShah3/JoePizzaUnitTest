7d24d2396c2e32e025b6be142b9b9ea35003d411
Vatsal Shah <vashah@teksystems.com>
2021-04-30 21:45:27 +0530

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestJoePizza")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestJoePizza")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3769790e-eee0-4e7a-8c81-4cf0934e79df")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace UnitTestJoePizza
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://vatsalazureappnew.azurewebsites.net/");
+                //driver.Navigate().GoToUrl("https://localhost:44391/");
+
+                new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(
+                    d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete")
+                    );
+
+                var cartBtn = driver.FindElementsByTagName("button");
+                Assert.IsNotNull(cartBtn);
+
+                var cartLink = driver.FindElementById("cartLink");
+                Assert.IsNotNull(cartLink);
+
+                var rand = new Random();
+                var clickButtonRandomly = rand.Next(0, 8);
+
+                cartBtn[clickButtonRandomly].Click();
+                cartLink.Click();
+
+                var cartTitle = driver.PageSource.Contains("My Cart");
+                Assert.IsTrue(cartTitle);
+
+                var email = driver.FindElementById("customerEmail");
+                Assert.IsNotNull(email);
+                email.SendKeys("vatsal@gmail.com");
+
+                var name = driver.FindElementById("customerName");
+                Assert.IsNotNull(name);
+                name.SendKeys("Vatsal Shah");
+
+                var address = driver.FindElementById("customerAddress");
+                Assert.IsNotNull(address);
+                address.SendKeys("Chunkin Street, Malad,  Mumbai");
+                
+                var pin = driver.FindElementById("customerPin");
+                Assert.IsNotNull(pin);
+                pin.SendKeys("123456");
+
+                var placeOrderBtn = driver.FindElementById("placeOrder");
+                Assert.IsNotNull(placeOrderBtn);
+                placeOrderBtn.Click();
+
+                var expectedUrl = "https://vatsalazureappnew.azurewebsites.net/OrderSuccess";
+                var redirectUrl = driver.Url;
+                Assert.AreEqual(expectedUrl, redirectUrl);
+
+                var orderTitle = driver.PageSource.Contains("Your order has been placed");
+                Assert.IsTrue(orderTitle);
+
+                var homeLinkBtn = driver.FindElementById("homeLink");
+                Assert.IsNotNull(homeLinkBtn);
+                homeLinkBtn.Click();
+
+                var expectedUrlForHome = "https://vatsalazureappnew.azurewebsites.net/";
+                var redirectUrlHome = driver.Url;
+                Assert.AreEqual(expectedUrlForHome, redirectUrlHome);
+
+                Console.WriteLine(cartBtn);
+            }
+        }
+    }
+}
